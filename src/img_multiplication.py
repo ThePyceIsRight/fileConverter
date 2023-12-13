30864e761b67bb0223964912893dd6dd95dbd9e4
lprice <loganprice.internet@gmail.com>
2023-12-13 16:04:32 -0600

    fixed the blur functionality

--- src/img_multiplication.py
+++ src/img_multiplication.py
@@ -2,35 +2,68 @@ import gradio as gr
 import cv2
 import numpy as np
 
-def blur_mask(data_dict):
 
+def view_im(image):
+    # Show the image (provide a window name)
+    cv2.imshow('image_window', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def handler():
+    image = cv2.imread("C:/Users/lprice/Downloads/oil-rig.jpg")
+    mask = cv2.imread("C:/Users/lprice/Downloads/mask.jpg")
+
+    mask_blurred = cv2.GaussianBlur(mask, (99, 99), 0)
+
+    image_blurred = cv2.GaussianBlur(image, (99, 99), 0)
+
+    mask_blurred_3chan = mask_blurred.astype('float') / 255.
+
+    img = image_blurred.astype('float') / 255.
+    bg = image.astype('float') / 255.
+
+    out = bg * (1 - mask_blurred_3chan) + img * mask_blurred_3chan
+
+    view_im(out)
+
+    return out
+
+
+# handler()
+
+
+def blur_mask(data_dict, blur_kernel, feather_kernel):
     image = data_dict["image"]
     mask = data_dict["mask"]
+    mask = mask[:, :, :3]
 
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-    mask_blur = cv2.GaussianBlur(mask_gray, (21, 21), 0)
+    mask_blurred = cv2.GaussianBlur(mask, (feather_kernel, feather_kernel), 0)
 
-    mask_bin = mask_blur / 255
+    image_blurred = cv2.GaussianBlur(image, (blur_kernel, blur_kernel), 0)
 
-    image_masked = image_gray * mask_bin
+    mask_blurred_3dim = mask_blurred.astype('float') / 255.
 
-    image_masked = image_masked.astype(np.uint8)
+    top_image = image_blurred.astype('float') / 255.
+    bottom_image = image.astype('float') / 255.
 
-    image_masked = cv2.cvtColor(image_masked, cv2.COLOR_GRAY2BGR)
+    final_image = bottom_image * (1 - mask_blurred_3dim) + top_image * mask_blurred_3dim
 
-    return image_masked
+    return final_image
 
 
 with gr.Blocks() as demo:
-    image_input = gr.Image(tool="sketch", label="input image", value="C:/Users/lprice/Downloads/Oil-rig.jpg")
+    with gr.Row():
+        image_input = gr.Image(tool="sketch", label="input image", value="C:/Users/lprice/Downloads/Oil-rig.jpg")
+        image_output = gr.Image(label="output image")
 
-    image_output = gr.Image(label="output image")
+    slider_blur = gr.Slider(value=99, label="Blur intensity")
+    slider_feather = gr.Slider(value=99, label="Feather size")
 
     button = gr.Button("process")
 
     button.click(fn=blur_mask,
-                 inputs=[image_input],
+                 inputs=[image_input, slider_blur, slider_feather],
                  outputs=image_output)
 
 demo.launch()
